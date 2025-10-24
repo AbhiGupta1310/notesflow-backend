3473087e7c0f29c53405b16d2570eba1e8831de7
Abhi Gupta <abhigupta5064@gmail.com>
2025-10-24 22:33:25 +0530

    fixing files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 10000;
 const JWT_SECRET = process.env.JWT_SECRET || "notesflow_secret";
 
 // Middleware
